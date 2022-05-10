8ca4283ac234be546cb74da2333e80c30f48ac7e
Trohat <petr.sed@centrum.cz>
2022-05-10 20:16:22 +0200

    Formuláře pomocí Django forms

--- knihy/forms.py
+++ knihy/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class KnihaForm(forms.Form):
-    jmeno = forms.CharField(max_length=200)
+    jmeno = forms.CharField(max_length=20, 
+                label="Zadej jméno knihy")
 
 
--- knihy/views.py
+++ knihy/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
 
 from .models import Kniha
+from .forms import KnihaForm
 # Create your views here.
 
 def nova_kniha(request):
     if request.method == "POST":
-        print("Formulář byl odeslán....")
-        jmeno_nove_knihy = request.POST["jmeno"]
-        if jmeno_nove_knihy != "":
-            nova_kniha = Kniha(jmeno=jmeno_nove_knihy)
+        form = KnihaForm(request.POST)
+        if form.is_valid():
+            jmeno = form.cleaned_data["jmeno"]
+            nova_kniha = Kniha(jmeno=jmeno)
             nova_kniha.save()
             return redirect("podekovani")
-    return render(request, "knihy/nova_kniha.html")
+    else:
+        form = KnihaForm()
+    context = {"form": form}
+    return render(request, "knihy/nova_kniha.html", context)
+
 
 def dekuji(request):
     return render(request, "knihy/dekuji.html")
